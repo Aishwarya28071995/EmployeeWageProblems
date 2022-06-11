c9408293e5158075b84650645f661cef0647c970
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-11 21:34:03 +0530

    Commiting files to UC2

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -5,27 +5,30 @@ namespace EmployeeWage
     internal class Program
     {
         static void Main(string[] args)
-        
+
+        {
+            Program program = new Program();
+            program.CalculateFullTimeWage();
+
+        }
+
+        public void CalculateFullTimeWage()
+        {
+            int WagePerHr = 20;
+            Random random = new Random();
+            int value = random.Next(0, 2);
+            if (value == 0)
             {
-                Program program = new Program();
-                program.EmployeeAttendance();
+                Console.WriteLine("Employee is absent");
+            }
+            else
+            {
+                int Salary = WagePerHr * 8;
+                Console.WriteLine(Salary);
             }
 
-            public void EmployeeAttendance()
-            {
-                Random random = new Random();
-                int value = random.Next(0, 2);
-                if (value == 0)
-                {
-                    Console.WriteLine("Employee is absent");
-                    Console.WriteLine(value);
-                }
-                else
-                {
-                    Console.WriteLine("Employee is present");
-                    Console.WriteLine(value);
-                }
-            }
+
         }
+    }
     }
 
