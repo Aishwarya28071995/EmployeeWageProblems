fe81a2ecbffee6e297a19476db8c78f8da3ba55f
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-11 21:30:49 +0530

    Commiting files to UC1

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -5,8 +5,27 @@ namespace EmployeeWage
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        
+            {
+                Program program = new Program();
+                program.EmployeeAttendance();
+            }
+
+            public void EmployeeAttendance()
+            {
+                Random random = new Random();
+                int value = random.Next(0, 2);
+                if (value == 0)
+                {
+                    Console.WriteLine("Employee is absent");
+                    Console.WriteLine(value);
+                }
+                else
+                {
+                    Console.WriteLine("Employee is present");
+                    Console.WriteLine(value);
+                }
+            }
         }
     }
-}
+
